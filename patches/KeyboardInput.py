e49b274cf596108689cff39f6e15240c77096da3
Arthur Mograbi <arthurmograbi@gmail.com>
2025-09-02 21:31:28 -0300

    KeyboardInput fixed

--- patches/KeyboardInput.py
+++ patches/KeyboardInput.py
@@ -29,9 +29,10 @@ class KeyboardInput(Patch):
         self.control = 0.0
         self.alt = 0.0
         
-        # Store pressed keys
+        # Store pressed keys and caps lock state
         self.pressed_keys = set()
         self.key_queue = deque(maxlen=10)  # Keep last 10 keys
+        self.caps_lock_active = False  # Track caps lock state
         
         # Setup keyboard listener
         self.listener = keyboard.Listener(
@@ -50,7 +51,9 @@ class KeyboardInput(Patch):
             else:
                 # Handle special keys
                 if key == keyboard.Key.caps_lock:
-                    self.caps = 1.0
+                    # Toggle caps lock state
+                    self.caps_lock_active = not self.caps_lock_active
+                    self.caps = 1.0 if self.caps_lock_active else 0.0
                 elif key == keyboard.Key.shift or key == keyboard.Key.shift_r:
                     self.shift = 1.0
                 elif key == keyboard.Key.ctrl or key == keyboard.Key.ctrl_r:
@@ -67,14 +70,12 @@ class KeyboardInput(Patch):
                 if char in self.pressed_keys:
                     self.pressed_keys.remove(char)
             else:
-                # Handle special keys
-                if key == keyboard.Key.caps_lock:
-                    self.caps = 0.0
-                elif key == keyboard.Key.shift or key == keyboard.Key.shift_r:
+                # Handle special keys (except caps lock which is a toggle)
+                if key == keyboard.Key.shift or key == keyboard.Key.shift_r:
                     self.shift = 0.0
-                elif key == keyboard.Key.ctrl or key == keyboard.Key.ctrl_r:
+                elif key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
                     self.control = 0.0
-                elif key == keyboard.Key.alt or key == keyboard.Key.alt_gr:
+                elif key == keyboard.Key.alt_l or key == keyboard.Key.alt_gr:
                     self.alt = 0.0
         except:
             pass
@@ -82,22 +83,29 @@ class KeyboardInput(Patch):
     def step(self):
         self.getInputs()
         
-        # Get the most recent key press
-        current_key = None
-        if self.key_queue:
-            current_key = self.key_queue[-1]
-        
-        # Handle chromatic layout
-        if current_key and current_key in self.chromatic_order:
-            self.chromatic_layout = self.chromatic_order.index(current_key) + 1  # +1 to avoid 0 when key is pressed
-        else:
+        # Reset outputs if no keys are currently pressed
+        if not self.pressed_keys:
             self.chromatic_layout = 0.0
-        
-        # Handle keyboard layout
-        if current_key and current_key in self.keyboard_order:
-            self.keyboard_layout = self.keyboard_order.index(current_key) + 1  # +1 to avoid 0 when key is pressed
-        else:
             self.keyboard_layout = 0.0
+        else:
+            # Get the most recent key press that is still pressed
+            current_key = None
+            for key in reversed(self.key_queue):
+                if key in self.pressed_keys:
+                    current_key = key
+                    break
+            
+            # Handle chromatic layout
+            if current_key and current_key in self.chromatic_order:
+                self.chromatic_layout = self.chromatic_order.index(current_key) + 1
+            else:
+                self.chromatic_layout = 0.0
+            
+            # Handle keyboard layout
+            if current_key and current_key in self.keyboard_order:
+                self.keyboard_layout = self.keyboard_order.index(current_key) + 1
+            else:
+                self.keyboard_layout = 0.0
 
         self.time += 1
 
